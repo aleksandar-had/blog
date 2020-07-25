90c062c7707a8a494ca42b4742d507d4345d5ef9
aleksandar-had <50676450+al-hadzhi@users.noreply.github.com>
2020-07-25 13:22:37 +0200

    User Avatars and post sub-template added additional commit

--- app/routes.py
+++ app/routes.py
@@ -66,7 +66,6 @@ def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     posts = [
         {"author": user, "body": "Test post #1"},
-        {"author": user, "body": "Test post #1"},
+        {"author": user, "body": "Test post #2"},
     ]
     return render_template("user.html", user=user, posts=posts)
-

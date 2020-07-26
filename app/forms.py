8a8f2aa85f52b1fa99f8e8667884030d4c66e0ee
aleksandar-had <50676450+al-hadzhi@users.noreply.github.com>
2020-07-26 11:42:48 +0200

    fixed same username bug in edit profile

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import TextAreaField
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from wtforms.validators import Length
 from app.models import User
 
 
@@ -30,9 +32,25 @@ class RegistrationForm(FlaskForm):
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
-            raise ValidationError("Username already taken,please use another one")
+            raise ValidationError("Username already taken,please use another")
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
-            raise ValidationError("Email already in use, please use another one")
+            raise ValidationError("Email already in use, please use another")
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    about_me = TextAreaField("About me", validators=[Length(min=0, max=140)])
+    submit = SubmitField("Submit")
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username_data).first()
+            if user is not None:
+                raise ValidationError("Please use a different username.")

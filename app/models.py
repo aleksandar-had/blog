6e0f1b6506213b9ba08dfbd499f9efb55d5e7275
aleksandar-had <50676450+al-hadzhi@users.noreply.github.com>
2020-07-26 19:34:44 +0200

    followers logic added

--- app/models.py
+++ app/models.py
@@ -6,14 +6,36 @@ from flask_login import UserMixin
 from hashlib import md5
 
 
+followers = db.Table(
+    "followers",
+    db.Column("follower_id", db.Integer, db.ForeignKey("user.id")),
+    db.Column("followed_id", db.Integer, db.ForeignKey("user.id")),
+)
+
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    # backref is how the relationship will be accessed from the RS entity
+    # lazy="dynamic" -> NO run until explicitly requested
     posts = db.relationship("Post", backref="author", lazy="dynamic")
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    # secondary configures the association table for this relationship
+    # primaryjoin links the LS entity(follower) with the association table
+    # followers.c(olumn).follower_id references the follower_id column
+    # primaryjoin links the RS entity(followed) with the association table
+    # relationship from LS entity - followed, from RS entity - followers
+    followed = db.relationship(
+        "User",
+        secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref("followers", lazy="dynamic"),
+        lazy="dynamic",
+    )
 
     def __repr__(self):
         return f"<User {self.username}>"

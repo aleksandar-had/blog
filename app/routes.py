3b7a7d99b857af9554ad74e132d387f5fb267416
aleksandar-had <50676450+al-hadzhi@users.noreply.github.com>
2020-07-27 16:52:45 +0200

    pagination

--- app/routes.py
+++ app/routes.py
@@ -31,8 +31,23 @@ def index():
         db.session.commit()
         flash("Your just posted something!")
         return redirect(url_for("index"))
-    posts = current_user.followed_posts().all()
-    return render_template("index.html", title="Home", form=form, posts=posts)
+    page = request.args.get("page", 1, type=int)
+    posts = current_user.followed_posts().paginate(
+        page, app.config["POSTS_PER_PAGE"], False
+    )
+    return render_template(
+        "index.html", title="Home", form=form, posts=posts.items
+    )
+
+
+@app.route("/explore")
+@login_required
+def explore():
+    page = request.args.get("page", 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, app.config["POSTS_PER_PAGE"], False
+    )
+    return render_template("index.html", title="Explore", posts=posts.items)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -141,10 +156,3 @@ def unfollow(username):
         return redirect(url_for("user", username=username))
     else:
         return redirect(url_for("index"))
-
-
-@app.route("/explore")
-@login_required
-def explore():
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template("index.html", title="Explore", posts=posts)

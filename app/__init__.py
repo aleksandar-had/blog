174252d2ea5f33e33ebbe1d190123088cd3ae1b4
aleksandar-had <50676450+al-hadzhi@users.noreply.github.com>
2020-07-29 10:48:16 +0200

    added server/email and file log error handling typos

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 
@@ -12,6 +13,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
+mail = Mail(app)
 login.login_view = "login"
 
 if not app.debug:
@@ -43,8 +45,10 @@ if not app.debug:
             "%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d"
         )
     )
+    file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
-    app.logger.setLevel(loggin.INFO)
+
+    app.logger.setLevel(logging.INFO)
     app.logger.info("Blog")
 
 from app import routes, models, errors  # noqa: E402

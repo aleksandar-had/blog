698e224fb02dff20c74af8b98c3a2021f1756aea
aleksandar-had <50676450+al-hadzhi@users.noreply.github.com>
2020-08-30 11:50:22 +0200

    boot.sh changes

--- boot.sh
+++ boot.sh
@@ -1,6 +1,7 @@
 # this script is used to boot a Docker container
 
+#!/bin/sh
 source venv/bin/activate
 flask db upgrade
 flask translate compile
-exec gunicorn -b :5000 --access-logfile - --error-logfile - blog:app
+exec gunicorn -b :5000 --access-logfile - --error-logfile - microblog:app

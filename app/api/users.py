5315012c7e6beba338da6abe5e0f45d33701ba80
aleksandar-had <50676450+al-hadzhi@users.noreply.github.com>
2020-09-28 14:01:20 +0200

    added user registration (POST) and user edit (PUT) API endpoints

--- app/api/users.py
+++ app/api/users.py
@@ -1,8 +1,9 @@
-from flask import jsonify
-from flask import json
+from flask import json, jsonify, url_for
 from flask.globals import request
+from app import db
 from app.models import User
 from app.api import bp
+from app.api.errors import bad_request
 
 
 @bp.route("/users/<int:id>", methods=["GET"])
@@ -43,9 +44,43 @@ def get_followed(id):
 
 @bp.route("/users", methods=["POST"])
 def create_user():
-    pass
+    data = request.get_json() or {}
+    if "username" not in data or "email" not in data or "password" not in data:
+        return bad_request("must include username, email and password fields")
+    if User.query.filter_by(username=data["username"]).first():
+        return bad_request(
+            "please use a different username, this is already taken"
+        )
+    if User.query.filter_by(email=data["email"]).first():
+        return bad_request(
+            "please use a different email address, this is already taken"
+        )
+    user = User()
+    user.from_dict(data, new_user=True)
+    db.session.add(user)
+    db.session.commit()
+    response = jsonify(user.to_dict())
+    response.status_code = 201
+    response.headers["Location"] = url_for("api.get_user", id=user.id)
+    return response
 
 
 @bp.route("/users/<int:id>", methods=["PUT"])
 def update_user(id):
-    pass
+    user = User.query.get_or_404(id)
+    data = request.get_json() or {}
+    if (
+        "username" in data
+        and data["username"] != user.username
+        and User.query.filter_by(username=data["username"]).first()
+    ):
+        return bad_request("please use a different username")
+    if (
+        "email" in data
+        and data["email"] != user.email
+        and User.query.filter_by(email=data["email"]).first()
+    ):
+        return bad_request("please use a different email address")
+    user.from_dict(data, new_user=False)
+    db.session.commit()
+    return jsonify(user.to_dict())
